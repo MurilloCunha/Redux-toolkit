54b898cd382a372716908c3738a44aa318f51564
MurilloCunha <62815895+MurilloCunha@users.noreply.github.com>
2023-10-20 12:50:08 -0300

    feat: add APi provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './Styles/global.css'
 import { RouterProvider } from 'react-router-dom'
 import router from '../routes';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { rickAndMortyAPI } from './APIs';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ApiProvider api={rickAndMortyAPI}>
+      <RouterProvider router={router} />
+    </ApiProvider>
   </React.StrictMode>,
 )
