a115aa85c837d8c6c5d1517f9b47cc908af5d1bd
MurilloCunha <62815895+MurilloCunha@users.noreply.github.com>
2023-10-20 12:50:21 -0300

    feat: add routes

--- routes.tsx
+++ routes.tsx
@@ -1,13 +1,13 @@
-import {Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import BaseLayout from "./src/layouts/BaseLayout";
-import { Homepage } from "./src/pages/Home";
-import { NotFound } from "./src/pages/NotFound";
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import BaseLayout from "./src/Layouts";
+import { CharactersPage, EpisodePage, LocationPage } from "./src/Pages";
 
   const routes =
-      <Route element={<BaseLayout />} errorElement={<NotFound />}>
-        <Route path={'/'} element={<Homepage />} />
+      <Route path='/' element={<BaseLayout />}>  
+        <Route path="Episode" element={<EpisodePage />} />
+        <Route path="Location" element={<LocationPage />} />
+        <Route path="Character" element={<CharactersPage />} />
       </Route>
-
 
 const router = createBrowserRouter(createRoutesFromElements(routes));
 
